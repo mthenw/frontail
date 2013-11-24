317240d02f7794f4133f547746f52abcd093e939
Maciej Winnicki <maciej.winnicki@gmail.com>
2013-11-24 13:55:47 +0100

    One level down

--- test/connect_builder.js
+++ test/connect_builder.js
@@ -1,109 +1,107 @@
+'use strict';
+
 var connectBuilder = require('../lib/connect_builder');
 var request = require('supertest');
 
-(function () {
-    'use strict';
-
-    describe('connectBuilder', function () {
-        it('should build connect app', function () {
-            connectBuilder().build().should.have.property('use');
-            connectBuilder().build().should.have.property('listen');
-        });
-
-        it('should build app requiring authorized user', function (done) {
-            var app = connectBuilder().authorize('user', 'pass').build();
-
-            request(app)
-                .get('/')
-                .expect('www-authenticate', 'Basic realm="Authorization Required"')
-                .expect(401, done);
-        });
-
-        it('should build app allowing user to login', function (done) {
-            var app = connectBuilder().authorize('user', 'pass').build();
-            app.use(function (req, res) {
-                res.end('secret!');
-            });
-
-            request(app)
-                .get('/')
-                .set('Authorization', 'Basic dXNlcjpwYXNz')
-                .expect(200, 'secret!', done);
-        });
-
-        it('should build app that setup session', function (done)  {
-            var app = connectBuilder().session('secret', 'sessionkey').build();
-            app.use(function (req, res) {
-                res.end();
-            });
-
-            request(app)
-                .get('/')
-                .expect('set-cookie', /^sessionkey/, done);
-        });
-
-        it('should build app that serve static files', function (done) {
-            var app = connectBuilder().static(__dirname + '/fixtures').build();
-
-            request(app)
-                .get('/foo')
-                .expect('bar', done);
-        });
-
-
-        it('should build app that serve index file', function (done) {
-            var app = connectBuilder().index(__dirname + '/fixtures/index', '/testfile').build();
-
-            request(app)
-                .get('/')
-                .expect(200)
-                .expect('Content-Type', 'text/html', done);
-        });
-
-        it('should build app that replace index title', function (done) {
-            var app = connectBuilder()
-                .index(__dirname + '/fixtures/index_with_title', '/testfile')
-                .build();
-
-            request(app)
-                .get('/')
-                .expect('<head><title>/testfile</title></head>', done);
-        });
-
-        it('builds app that sets socket.io namespace based on files', function (done) {
-            var app = connectBuilder()
-                .index(__dirname + '/fixtures/index_with_ns', '/testfile', 'ns', 'dark')
-                .build();
-
-            request(app)
-                .get('/')
-                .expect('ns', done);
-        });
-
-        it('should build app that sets theme', function (done) {
-            var app = connectBuilder()
-                .index(__dirname + '/fixtures/index_with_theme', '/testfile', 'ns', 'dark')
-                .build();
-
-            request(app)
-                .get('/')
-                .expect(
-                    '<head><title>/testfile</title><link href="dark.css" rel="stylesheet" type="text/css"/></head>',
-                    done
-                );
-        });
-
-        it('should build app that sets default theme', function (done) {
-            var app = connectBuilder()
-                .index(__dirname + '/fixtures/index_with_theme', '/testfile')
-                .build();
-
-            request(app)
-                .get('/')
-                .expect(
-                    '<head><title>/testfile</title><link href="default.css" rel="stylesheet" type="text/css"/></head>',
-                    done
-                );
-        });
+describe('connectBuilder', function () {
+    it('should build connect app', function () {
+        connectBuilder().build().should.have.property('use');
+        connectBuilder().build().should.have.property('listen');
     });
-})();
+
+    it('should build app requiring authorized user', function (done) {
+        var app = connectBuilder().authorize('user', 'pass').build();
+
+        request(app)
+            .get('/')
+            .expect('www-authenticate', 'Basic realm="Authorization Required"')
+            .expect(401, done);
+    });
+
+    it('should build app allowing user to login', function (done) {
+        var app = connectBuilder().authorize('user', 'pass').build();
+        app.use(function (req, res) {
+            res.end('secret!');
+        });
+
+        request(app)
+            .get('/')
+            .set('Authorization', 'Basic dXNlcjpwYXNz')
+            .expect(200, 'secret!', done);
+    });
+
+    it('should build app that setup session', function (done)  {
+        var app = connectBuilder().session('secret', 'sessionkey').build();
+        app.use(function (req, res) {
+            res.end();
+        });
+
+        request(app)
+            .get('/')
+            .expect('set-cookie', /^sessionkey/, done);
+    });
+
+    it('should build app that serve static files', function (done) {
+        var app = connectBuilder().static(__dirname + '/fixtures').build();
+
+        request(app)
+            .get('/foo')
+            .expect('bar', done);
+    });
+
+
+    it('should build app that serve index file', function (done) {
+        var app = connectBuilder().index(__dirname + '/fixtures/index', '/testfile').build();
+
+        request(app)
+            .get('/')
+            .expect(200)
+            .expect('Content-Type', 'text/html', done);
+    });
+
+    it('should build app that replace index title', function (done) {
+        var app = connectBuilder()
+            .index(__dirname + '/fixtures/index_with_title', '/testfile')
+            .build();
+
+        request(app)
+            .get('/')
+            .expect('<head><title>/testfile</title></head>', done);
+    });
+
+    it('builds app that sets socket.io namespace based on files', function (done) {
+        var app = connectBuilder()
+            .index(__dirname + '/fixtures/index_with_ns', '/testfile', 'ns', 'dark')
+            .build();
+
+        request(app)
+            .get('/')
+            .expect('ns', done);
+    });
+
+    it('should build app that sets theme', function (done) {
+        var app = connectBuilder()
+            .index(__dirname + '/fixtures/index_with_theme', '/testfile', 'ns', 'dark')
+            .build();
+
+        request(app)
+            .get('/')
+            .expect(
+                '<head><title>/testfile</title><link href="dark.css" rel="stylesheet" type="text/css"/></head>',
+                done
+            );
+    });
+
+    it('should build app that sets default theme', function (done) {
+        var app = connectBuilder()
+            .index(__dirname + '/fixtures/index_with_theme', '/testfile')
+            .build();
+
+        request(app)
+            .get('/')
+            .expect(
+                '<head><title>/testfile</title><link href="default.css" rel="stylesheet" type="text/css"/></head>',
+                done
+            );
+    });
+});
--- test/server_builder.js
+++ test/server_builder.js
@@ -1,115 +1,113 @@
+'use strict';
+
 var fs = require('fs');
 var http = require('http');
 var https = require('https');
 var serverBuilder = require('../lib/server_builder');
 var sinon = require('sinon');
 
-(function () {
-    'use strict';
+describe('serverBuilder', function () {
 
-    describe('serverBuilder', function () {
+    describe('http server', function () {
+        var httpServer;
+        var createServer;
 
-        describe('http server', function () {
-            var httpServer;
-            var createServer;
-
-            beforeEach(function () {
-                httpServer = sinon.createStubInstance(http.Server);
-                httpServer.listen.returns(httpServer);
-                createServer = sinon.stub(http, 'createServer').returns(httpServer);
-            });
-
-            afterEach(function () {
-                createServer.restore();
-            });
-
-            it('should build server', function () {
-                var server = serverBuilder().build();
-
-                createServer.calledOnce.should.equal(true);
-                server.should.be.an.instanceof(http.Server);
-            });
-
-            it('should build server accepting requests', function () {
-                var callback = function () {};
-
-                serverBuilder().use(callback).build();
-
-                createServer.calledWith(callback).should.equal(true);
-            });
-
-            it('should build listening server', function () {
-                serverBuilder().build();
-
-                httpServer.listen.calledOnce.should.equal(true);
-            });
-
-            it('should build server listening on specified port', function () {
-                serverBuilder().port(6666).build();
-
-                httpServer.listen.calledWith(6666).should.equal(true);
-            });
-
-            it('should build server listening on default port', function () {
-                serverBuilder().build();
-
-                httpServer.listen.calledWith(9001).should.equal(true);
-            });
-
-            it('should build server listening on specified host', function () {
-                serverBuilder().host('127.0.0.1').build();
-
-                httpServer.listen.calledWith(9001, '127.0.0.1').should.equal(true);
-            });
-
-            it('should build server listening on default host', function () {
-                serverBuilder().build();
-
-                httpServer.listen.calledWith(9001, null).should.equal(true);
-            });
+        beforeEach(function () {
+            httpServer = sinon.createStubInstance(http.Server);
+            httpServer.listen.returns(httpServer);
+            createServer = sinon.stub(http, 'createServer').returns(httpServer);
         });
 
-        describe('https server', function () {
-            var httpsServer;
-            var createHttpsServer;
-            var readFileSyncStub;
+        afterEach(function () {
+            createServer.restore();
+        });
 
-            beforeEach(function () {
-                httpsServer = sinon.createStubInstance(https.Server);
-                httpsServer.listen.returns(httpsServer);
-                createHttpsServer = sinon.stub(https, 'createServer').returns(httpsServer);
-                readFileSyncStub = sinon.stub(fs, 'readFileSync');
-                readFileSyncStub.withArgs('key.pem').returns('testkey');
-                readFileSyncStub.withArgs('cert.pem').returns('testcert');
-            });
+        it('should build server', function () {
+            var server = serverBuilder().build();
 
-            afterEach(function () {
-                createHttpsServer.restore();
-                readFileSyncStub.restore();
-            });
+            createServer.calledOnce.should.equal(true);
+            server.should.be.an.instanceof(http.Server);
+        });
 
-            it('should build server', function () {
-                var server = serverBuilder().secure('key.pem', 'cert.pem').build();
+        it('should build server accepting requests', function () {
+            var callback = function () {};
 
-                server.should.be.an.instanceof(https.Server);
-                createHttpsServer.calledWith({key: 'testkey', cert: 'testcert'}).should.equal(true);
-            });
+            serverBuilder().use(callback).build();
 
-            it('should build server accepting requests', function () {
-                var callback = function () {};
+            createServer.calledWith(callback).should.equal(true);
+        });
 
-                serverBuilder().use(callback).secure('key.pem', 'cert.pem').build();
+        it('should build listening server', function () {
+            serverBuilder().build();
 
-                createHttpsServer.calledWith({key: 'testkey', cert: 'testcert'}, callback).should.equal(true);
-            });
+            httpServer.listen.calledOnce.should.equal(true);
+        });
 
-            it('should throw error if key or cert not provided', function () {
-                readFileSyncStub.restore();
+        it('should build server listening on specified port', function () {
+            serverBuilder().port(6666).build();
 
-                (function () {
-                    serverBuilder().secure('nofile', 'nofile');
-                }).should.throw('No key or certificate file found');
-            });
+            httpServer.listen.calledWith(6666).should.equal(true);
+        });
+
+        it('should build server listening on default port', function () {
+            serverBuilder().build();
+
+            httpServer.listen.calledWith(9001).should.equal(true);
+        });
+
+        it('should build server listening on specified host', function () {
+            serverBuilder().host('127.0.0.1').build();
+
+            httpServer.listen.calledWith(9001, '127.0.0.1').should.equal(true);
+        });
+
+        it('should build server listening on default host', function () {
+            serverBuilder().build();
+
+            httpServer.listen.calledWith(9001, null).should.equal(true);
         });
     });
-})();
+
+    describe('https server', function () {
+        var httpsServer;
+        var createHttpsServer;
+        var readFileSyncStub;
+
+        beforeEach(function () {
+            httpsServer = sinon.createStubInstance(https.Server);
+            httpsServer.listen.returns(httpsServer);
+            createHttpsServer = sinon.stub(https, 'createServer').returns(httpsServer);
+            readFileSyncStub = sinon.stub(fs, 'readFileSync');
+            readFileSyncStub.withArgs('key.pem').returns('testkey');
+            readFileSyncStub.withArgs('cert.pem').returns('testcert');
+        });
+
+        afterEach(function () {
+            createHttpsServer.restore();
+            readFileSyncStub.restore();
+        });
+
+        it('should build server', function () {
+            var server = serverBuilder().secure('key.pem', 'cert.pem').build();
+
+            server.should.be.an.instanceof(https.Server);
+            createHttpsServer.calledWith({key: 'testkey', cert: 'testcert'}).should.equal(true);
+        });
+
+        it('should build server accepting requests', function () {
+            var callback = function () {};
+
+            serverBuilder().use(callback).secure('key.pem', 'cert.pem').build();
+
+            createHttpsServer.calledWith({key: 'testkey', cert: 'testcert'}, callback).should.equal(true);
+        });
+
+        it('should throw error if key or cert not provided', function () {
+            readFileSyncStub.restore();
+
+            (function () {
+                serverBuilder().secure('nofile', 'nofile');
+            }).should.throw('No key or certificate file found');
+        });
+    });
+});

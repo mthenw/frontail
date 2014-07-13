78076c1b8e9855241c8beaef42490c900cd385ff
Maciej Winnicki <maciej.winnicki@gmail.com>
2014-07-13 02:42:21 +0200

    Buffer lines on start (fixes #33)

--- test/tail.js
+++ test/tail.js
@@ -5,37 +5,51 @@ var fs = require('fs');
 var tail = require('../lib/tail');
 var temp = require('temp');
 
+var TEMP_FILE_PROFIX = '';
+var SPAWN_DELAY = 10;
+
 describe('tail', function () {
     temp.track();
 
     it('calls event line if new line appear in file', function (done) {
-        temp.open('', function (err, info) {
+        temp.open(TEMP_FILE_PROFIX, function (err, info) {
             tail(info.path).on('line', function (line) {
-                line.should.equal('testline');
+                line.should.equal('line0');
                 done();
             });
 
-            fs.writeSync(info.fd, 'testline\n');
-            fs.closeSync(info.fd);
+            setTimeout(writeLines, SPAWN_DELAY, info.fd, 1);
         });
     });
 
     it('buffers lines on start', function (done) {
-        temp.open('', function (err, info) {
-            fs.writeSync(info.fd, 'testline1\n');
-            fs.writeSync(info.fd, 'testline2\n');
-            fs.writeSync(info.fd, 'testline3\n');
-            fs.closeSync(info.fd);
+        temp.open(TEMP_FILE_PROFIX, function (err, info) {
+            writeLines(info.fd, 20)
 
-            var calls = 0;
-            var tailer = tail(info.path, {buffer: 2}).on('line', function () {
-                calls += 1;
-
-                if (calls === 3) {
-                    tailer.getBuffer().should.eql(['testline2', 'testline3']);
-                    done();
-                }
-            });
+            var tailer = tail(info.path, {buffer: 2});
+            setTimeout(function () {
+                tailer.getBuffer().should.be.eql(['line18', 'line19']);
+                done();
+            }, SPAWN_DELAY);
         });
     });
+
+    it('buffers no lines on start by default', function (done) {
+        temp.open(TEMP_FILE_PROFIX, function (err, info) {
+            writeLines(info.fd, 3)
+
+            var tailer = tail(info.path);
+            setTimeout(function () {
+                tailer.getBuffer().should.be.empty;
+                done()
+            }, SPAWN_DELAY);
+        });
+    });
+
+    function writeLines(fd, count) {
+        for (var i = 0; i < count; i += 1) {
+            fs.writeSync(fd, 'line' + i + '\n');
+        }
+        fs.closeSync(fd);
+    }
 });

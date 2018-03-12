095cc8816e03b3124a1c503d5bacff75be52521f
Liam Bilich <syberkitten@gmail.com>
2018-03-12 17:52:00 +0200

    adding epipebomb to make sure no broker pipes happen

--- index.js
+++ index.js
@@ -22,6 +22,29 @@ if (program.args.length === 0) {
   process.exit();
 }
 
+function epipeBomb(stream, callback) {
+    if (stream == null) stream = process.stdout
+    if (callback == null) callback = process.exit
+
+    function epipeFilter(err) {
+        if (err.code === 'EPIPE') return callback()
+
+        // If there's more than one error handler (ie, us),
+        // then the error won't be bubbled up anyway
+        if (stream.listeners('error').length <= 1) {
+            stream.removeAllListeners()     // Pretend we were never here
+            stream.emit('error', err)       // Then emit as if we were never here
+            stream.on('error', epipeFilter) // Then reattach, ready for the next error!
+        }
+    }
+
+    stream.on('error', epipeFilter)
+}
+
+// make sure no broken pipes / e-pipe
+epipeBomb()
+
+
 /**
  * Validate params
  */
